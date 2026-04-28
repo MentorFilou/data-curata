eed090c8b83fa63eb945c4554d7421da726031fa
LennarX <mentorfilou@gmail.com>
2026-04-28 21:31:02 +0200

    chore: update playwright config to run in parallel

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig, devices } from '@playwright/test'
 
+// Config inspired by https://playwright.dev/docs/test-configuration
 export default defineConfig({
   testDir: './tests/e2e',
-  fullyParallel: false,
+  fullyParallel: true,
+  // failing if test.only is used in CI
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
-  workers: 1,
+  workers: process.env.CI ? 1 : undefined,
   reporter: 'html',
   use: {
     baseURL: 'http://localhost:5173',
