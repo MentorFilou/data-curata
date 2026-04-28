19ce8cc3171ea960cbabb2a0bcc2a60a2c3c1851
LennarX <mentorfilou@gmail.com>
2026-04-28 22:48:51 +0200

    feat: use dynamic imports per page to reduce file size for performance

--- src/router.ts
+++ src/router.ts
@@ -1,18 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import CollectPage from '@/pages/CollectPage.vue'
-import DefinePage from '@/pages/DefinePage.vue'
-import HomePage from './pages/HomePage.vue'
-import ReviewPage from './pages/ReviewPage.vue'
-import DocsPage from './pages/DocsPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    { path: '/', name: 'home', component: HomePage },
-    { path: '/define', name: 'define', component: DefinePage },
-    { path: '/collect', name: 'collect', component: CollectPage },
-    { path: '/review', name: 'review', component: ReviewPage },
-    { path: '/docs/:pathMatch(.*)*', name: 'docs', component: DocsPage },
+    { path: '/', name: 'home', component: () => import('@/pages/HomePage.vue') },
+    { path: '/define', name: 'define', component: () => import('@/pages/DefinePage.vue') },
+    { path: '/collect', name: 'collect', component: () => import('@/pages/CollectPage.vue') },
+    { path: '/review', name: 'review', component: () => import('@/pages/ReviewPage.vue') },
+    {
+      path: '/docs/:pathMatch(.*)*',
+      name: 'docs',
+      component: () => import('@/pages/DocsPage.vue'),
+    },
   ],
 })
 
